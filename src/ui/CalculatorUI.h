8ae020df4330afe59d00552a8d40748c0d3b7e64
洋 李 <lyxm19890327@126.com>
2019-10-19 16:56:45 +0800

    删除冗余代码

--- src/ui/CalculatorUI.h
+++ src/ui/CalculatorUI.h
@@ -13,17 +13,6 @@ private:
 	void onKeyButtonClicked(int row, int column);
 
 private:
-	/*const int row = 5;
-	const int column = 4;
-	QString m_KeyDict[5][4] =
-	{
-		{ "(", ")", "<-", "*" },
-		{ "7", "8", "9", "/" },
-		{ "4", "5", "6", "+" },
-		{ "1", "2", "3", "-" },
-		{ "CE", "0", ".", "=" }
-	};*/
-
 	static const int row = 7;
 	static const int column = 5;
 	QString m_KeyDict[row][column] =
